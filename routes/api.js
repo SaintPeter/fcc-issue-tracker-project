d4d9ca4a822d3bd6d06c4688d247a13ccac5deb5
SaintPeter <rex.schrader@gmail.com>
2020-09-18 19:25:34 -0700

    POST Complete

--- routes/api.js
+++ routes/api.js
@@ -11,6 +11,13 @@
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
 var ObjectId = require('mongodb').ObjectID;
+var IssueModel = require('../models/issue').IssueModel;
+
+const mongoose = require('mongoose');
+const connection = mongoose.connect('mongodb://localhost:27017/issue_tracker', {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.set('useCreateIndex', true);
+
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
@@ -20,12 +27,44 @@ module.exports = function (app) {
   
     .get(function (req, res){
       var project = req.params.project;
-      
+
+      console.log('/api/issues/' + project);
+      IssueModel.find({ project: project }, (err, docs) => {
+        console.log("Docs Found: ",docs.length)
+        return res.json(docs);
+      })
     })
     
     .post(function (req, res){
-      var project = req.params.project;
-      
+      let project = req.params.project;
+
+      console.log("Post to Project: " + project);
+      // check required fields
+      let missing_fields = ['issue_title', 'issue_text', 'created_by']
+        .filter( field => !req.body.hasOwnProperty(field))
+        .join(',');
+
+      if(missing_fields) {
+        return res.send("required fields missing: " + missing_fields);
+      }
+
+      // Otherwise, create the object
+      let issue = new IssueModel({
+        project: project,
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to  || '',
+        status_text: req.body.status_text || ''
+      });
+
+      issue.save(function(err, result) {
+        console.log("Saved");
+        if(err) { console.log(err) }
+        console.log("Doc Written:",result.toJSON);
+        res.json(result.toJSON());
+      })
+
     })
     
     .put(function (req, res){
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -18,30 +18,61 @@ suite('Functional Tests', function() {
     suite('POST /api/issues/{project} => object with issue data', function() {
       
       test('Every field filled in', function(done) {
-       chai.request(server)
-        .post('/api/issues/test')
-        .send({
+        let issue_data = {
           issue_title: 'Title',
           issue_text: 'text',
           created_by: 'Functional Test - Every field filled in',
           assigned_to: 'Chai and Mocha',
           status_text: 'In QA'
-        })
+        }
+       chai.request(server)
+        .post('/api/issues/test')
+        .send(issue_data)
         .end(function(err, res){
           assert.equal(res.status, 200);
-          
-          //fill me in too!
-          
+          assert.nestedInclude(res.body, issue_data)
+          assert.isDefined(res.body.updated_on)
+          assert.isDefined(res.body.created_on)
+          assert.equal(res.body.updated_on,res.body.created_on,"Created and Updated Dates should match" )
+          assert.isDefined(res.body.open)
+          assert.deepEqual(res.body.open,true);
           done();
         });
       });
       
       test('Required fields filled in', function(done) {
-        
+        let issue_data = {
+          issue_title: 'Title - Required',
+          issue_text: 'text - Required',
+          created_by: 'Functional Test - Required',
+        }
+        chai.request(server)
+          .post('/api/issues/test')
+          .send(issue_data)
+          .end(function(err, res){
+            assert.equal(res.status, 200);
+            assert.nestedInclude(res.body, issue_data)
+            assert.isDefined(res.body.updated_on)
+            assert.isDefined(res.body.created_on)
+            assert.equal(res.body.updated_on,res.body.created_on,"Created and Updated Dates should match" )
+            assert.isDefined(res.body.open)
+            assert.deepEqual(res.body.open,true);
+            done();
+          });
       });
       
       test('Missing required fields', function(done) {
-        
+        let issue_data = {
+          issue_title: 'Title'
+        }
+        chai.request(server)
+          .post('/api/issues/test')
+          .send(issue_data)
+          .end(function(err, res){
+            assert.equal(res.status, 200);
+            assert.equal(res.text,"required fields missing: issue_text,created_by")
+            done();
+          });
       });
       
     });
